21d9e12275c29b7d761f30afdce0a4c0a189b601
gritarmy <micah@gritarmy.com>
2025-12-15 19:07:47 -0600

    Create config.js

---
+++ scripts/config.js
@@ -0,0 +1,8 @@
+export const PAGES = [
+  "https://gritarmy.com/",
+  "https://gritarmy.com/collections/socks",
+  "https://gritarmy.com/cart"
+];
+
+// Optional: if you add Google Sheets later
+export const GOOGLE_SHEETS_WEBHOOK_URL = process.env.GS_WEBHOOK_URL || "";
