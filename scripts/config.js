f9f605a7a392120aa908e7c5c1a9c9e5e41da359
gritarmy <micah@gritarmy.com>
2026-01-07 09:51:36 -0600

    Update config.js

--- scripts/config.js
+++ scripts/config.js
@@ -3,7 +3,8 @@ export const PAGES = [
   "https://gritarmy.com/collections/socks",
   "https://gritarmy.com/cart",
   "https://gritarmy.com/products/high-compression-socks-adult",
-  "https://gritarmy.com/products/premium-athletic-crew-socks"
+  "https://gritarmy.com/products/premium-athletic-crew-socks",
+  "https://gritarmy.com/products/grit-army-trifecta"
 ];
 
 // Optional: if you add Google Sheets later
